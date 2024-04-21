9e48bdb2886ddfb45fbe0018cc5b9fa471a2ee90
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-04-21 12:50:22 +0900

    푸터 애니메이션 추가 완료

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+"use client"
+
+import { useRef } from 'react'
+import { useScroll, useTransform, motion } from 'framer-motion'
 
 export default function Footer() {
+  const container = useRef<HTMLElement | null>(null)
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end end']
+  })
+  const y = useTransform(scrollYProgress, [0, 1], [-225, 0])
   return (
-    <div>Footer</div>
+    <footer id='footer' ref={container}>
+      <div className="h-[250px] bg-black overflow-hidden">
+        <motion.div style={{ y }} className="h-full bg-black flex justify-center gap-10 items-center p-10">
+          {
+            <ul>
+              <li className="text-white">푸터컨텐츠</li>
+            </ul>
+          }
+        </motion.div>
+      </div>
+    </footer>
   )
 }
