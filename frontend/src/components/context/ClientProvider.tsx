4b48dbd622db4c0f8a6363ba32e04a96fd0051b2
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-09-03 22:29:47 +0900

    수정

--- frontend/src/components/context/ClientProvider.tsx
+++ frontend/src/components/context/ClientProvider.tsx
@@ -1,24 +1,39 @@
-'use client';
+"use client";
 
-import { useRef } from "react";
+import {useRef} from "react";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import StickyCursor from "../interactive/StickyCursor";
 import useDeviceSettings from "@/hooks/useDeviceSettings";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-export default function ClientProvider({ children }: Props) {
-  const stickyElement = useRef(null)
-  const { isLargeEnough, isMobile } = useDeviceSettings();
+export default function ClientProvider({
+  children,
+}: Props) {
+  const stickyElement =
+    useRef(null);
+  const {
+    isLargeEnough,
+    isMobile,
+  } = useDeviceSettings();
   return (
     <>
-      <Header ref={stickyElement} />
+      <Header
+        ref={stickyElement}
+      />
       {children}
       <Footer />
-      {isLargeEnough && !isMobile && <StickyCursor stickyElement={stickyElement} />}
+      {isLargeEnough &&
+        !isMobile && (
+          <StickyCursor
+            stickyElement={
+              stickyElement
+            }
+          />
+        )}
     </>
-  )
+  );
 }
